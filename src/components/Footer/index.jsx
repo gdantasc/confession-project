f3ce5ca3d23fa031edb7a153d3306c371834e114
gdantasc <gabrieldcavalcante00@gmail.com>
2022-02-07 12:48:19 -0300

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ul>
+                <li>Fale Conosco</li>
+                <li>Shjsdgahjs</li>
+            </ul>
+            <p>Costs 2021</p>
+        </footer>
+    )
+}
+
+
+export default Footer
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css"
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>é isso</h1>
+        </div>
+
+    )
+}
+
+
+export default Home
